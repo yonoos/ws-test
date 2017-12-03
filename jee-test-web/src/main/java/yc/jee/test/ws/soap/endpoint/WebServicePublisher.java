bd95788f73bc60e3f5d72002122f5d5c09a8abe2
yonoos <34213455+yonoos@users.noreply.github.com>
2017-12-03 23:16:25 +0100

    xcw
    xwc

--- jee-test-web/src/main/java/yc/jee/test/ws/soap/endpoint/WebServicePublisher.java
+++ jee-test-web/src/main/java/yc/jee/test/ws/soap/endpoint/WebServicePublisher.java
@@ -2,7 +2,7 @@ package yc.jee.test.ws.soap.endpoint;
 
 import javax.xml.ws.Endpoint;
 
-public class WebServicePublisher {
+public class WebServicePublisher {  
 
 	public static void main(String[] args) {
 		 Endpoint.publish("http://localhost:9991/ws/SumService", new WebServiceSum());
